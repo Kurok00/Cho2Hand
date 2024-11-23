a244da003f468750e42508235e598aaf2b64a4e6
Kurok00 <90438576+Kurok00@users.noreply.github.com>
2024-11-24 05:55:07 +0700

    updatee dockker file

--- server/main.go
+++ server/main.go
@@ -9,15 +9,9 @@ import (
 	"cho2hand/middleware"
 	"cho2hand/routes"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load environment variables from .env file
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	// Debug: Print environment variables
 	log.Println("MONGO_URI:", os.Getenv("MONGO_URI"))
 	log.Println("REDIS_URI:", os.Getenv("REDIS_URI"))
